c932d7dcf04a39b4f342dc628c579a63c027467a
shusbox <shusbox@naver.com>
2025-09-15 15:31:33 +0900

    흠

--- src/components/musicPlayer.jsx
+++ src/components/musicPlayer.jsx
@@ -1,9 +1,10 @@
-import { Shuffle, Loop, Pause, Play, Previous } from '../assets/icons.jsx'
+import { Shuffle, Loop, Pause, Play, Previous, NowPlaying, Lyrics, Playlist, Sound, PIP, FullScreen, Connect } from '../assets/icons.jsx'
 import '../css/musicPlayer.css'
 
 function MusicPlayer() {
   return (
     <div id='music-player'>
+      <div></div>
       <div id='playing-selection'>
         <div id='player-controls'>
           <div className='player-icons'> <Shuffle size={"16"} /> </div>
@@ -13,10 +14,22 @@ function MusicPlayer() {
           <div className='player-icons'> <Loop size={"16"} /> </div>
         </div>
         <div id='player-bar'>
-          <p> -:-- </p>
-          <div id='player-bar-status'></div>
-          <p> -:-- </p>
+          <p className='player-bar-time'> -:-- </p>
+          <div className='controls-bar'></div>
+          <p className='player-bar-time'> -:-- </p>
         </div>
+      </div>
+      <div id='right-controls'>
+        <div className='player-icons'> <NowPlaying size={"16"} /> </div>
+        <div className='player-icons'> <Lyrics size={"16"} /> </div>
+        <div className='player-icons'> <Playlist size={"16"} /> </div>
+        <div className='player-icons'> <Connect size={"16"} /> </div>
+        <div id='right-sound-controls'>
+          <div className='player-icons'> <Sound size={"16"} /> </div>
+          <div id='controls-bar-sound' className='controls-bar' /> {/* 아이거어떡하지 */}
+        </div>
+        <div className='player-icons'> <PIP size={"16"} /> </div>
+        <div className='player-icons'> <FullScreen size={"16"} /> </div>
       </div>
     </div>
   )
