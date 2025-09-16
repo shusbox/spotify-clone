8573071e15e6d905f818475af4c4718608ff3231
shusbox <shusbox@naver.com>
2025-09-16 19:50:19 +0900

    씁

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import Header from './components/header.jsx'
 import Index from './pages/index.jsx'
-import MusicPlayer from './components/musicPlayer.jsx'
 import './css/app.css'
 
 function App() {
   return (
-    <main>
-      <Header />
+    <>
       <Index />
-      <MusicPlayer />
-    </main>
+    </>
   )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,59 @@
+import Header from '../components/header.jsx'
+import MusicPlayer from '../components/musicPlayer.jsx'
+import { Add, Hide } from '../assets/icons.jsx'
 import '../css/main.css'
+
+function Library() {
+  function LibraryBox({ title, txt, btn }) {
+    return (
+      <div className='library-box'>
+        <div className='library-box-title'>
+          <h3> {title} </h3>
+          <p> {txt} </p>
+        </div>
+        <div className='library-box-btn'> <p> {btn} </p> </div>
+      </div>
+    )
+  }
+
+  return (
+    <div id='library'>
+      <div id='library-title'>
+        <div id='library-title-txt-container'>
+          <div id='library-title-txt-icons'>
+            <Hide size={"16"} />
+          </div>
+          <h2 id='library-title-txt'> 내 라이브러리 </h2>
+        </div>
+        <div id='library-create'>
+          <Add size={"16"} />
+          <p id='library-create-txt'> 만들기 </p>
+        </div>
+      </div>
+      <div id='library-container'>
+        <LibraryBox
+          title={"첫 번째 플레이리스트를 만드세요."}
+          txt={"어렵지 않아요, 저희가 도와드릴게요."}
+          btn={"플레이리스트 만들기"}
+        />
+        <LibraryBox
+          title={"팔로우할 팟캐스트를 찾아보세요"}
+          txt={"새로운 에피소드에 대한 소식을 알려드릴게요."}
+          btn={"팟캐스트 둘러보기"}
+        />
+      </div>
+    </div>
+  )
+}
 
 function Index() {
   return (
-    <div id='container'>
-      
+    <div id='index'>
+      <Header />
+      <main>
+        <Library />
+      </main>
+      <MusicPlayer />
     </div>
   )
 }
