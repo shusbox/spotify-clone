998e8cbf51d64f5bd905d871c563b209b3348b2d
shusbox <shusbox@naver.com>
2025-09-16 23:20:06 +0900

    하....레전드리펙터링

--- src/components/musicPlayer.jsx
+++ src/components/musicPlayer.jsx
@@ -2,17 +2,49 @@ import { Shuffle, Loop, Pause, Start, Play, Previous, NowPlaying, Lyrics, Playli
 import '../css/musicPlayer.css'
 
 function MusicPlayer() {
+  const player_controls_arr = [
+    { type: "icon", icon: Shuffle, txt: "셔플 활성화하기" },
+    { type: "icon", icon: Play, txt: "이전" },
+    { type: "play", icon: Start, txt: "재생하기" },
+    { type: "icon", icon: Previous, txt: "다음" },
+    { type: "icon", icon: Loop, txt: "반복 활성화하기" }
+  ]
+
+  const right_controls_arr = [
+    { type: "icon", icon: NowPlaying, txt: "지금 재생 중 보기" },
+    { type: "icon", icon: Lyrics, txt: "가사" },
+    { type: "icon", icon: Playlist, txt: "재생목록" },
+    { type: "icon", icon: Connect, txt: "기기에 연결하기" },
+    { type: "sound", icon: Sound, txt: "음소거하기" },
+    { type: "icon", icon: PIP, txt: "미니플레이어 열기" },
+    { type: "icon", icon: FullScreen, txt: "전체 화면으로 전환" }
+  ];
+
+  const player_controls = player_controls_arr.map((item, index) => {
+    const Comp = item.icon;
+    if (item.type === "icon") return <div className='player-icons tooltip' tooltip-txt={item.txt}> <Comp size={"16"} /> </div>
+    if (item.type === "play") return <div id='player-icons-pause' className='player-icons music-player-icons tooltip' tooltip-txt={item.txt}> <Comp size={"16"} /> </div>
+  })
+
+  const right_controls = right_controls_arr.map((item, index) => {
+    const Comp = item.icon;
+    if (item.type === "icon") return <div key={index} className='player-icons tooltip' tooltip-txt={item.txt}> <Comp size={"16"} /> </div>
+    if (item.type === "sound") {
+      return(
+        <div id='right-sound-controls'>
+          <div className='player-icons tooltip' tooltip-txt={item.txt}> <Comp size={"16"} /> </div>
+          <div key={index} id='controls-bar-sound' className='controls-bar' /> {/* 아이거어떡하지 */}
+        </div>
+      )
+    }
+  });
+
   return (
     <div id='music-player'>
       <div></div>
       <div id='playing-selection'>
         <div id='player-controls'>
-          <div className='player-icons tooltip' tooltip-txt='셔플 활성화하기'> <Shuffle size={"16"} /> </div>
-          <div className='player-icons tooltip' tooltip-txt='이전'> <Play size={"16"} /> </div>
-          {/* 누르면 시작하고 더ㅏ시 누르면 멈출수있게 멈추면 아이콘이랑 content도 바뀌게 */}
-          <div id='player-icons-pause' className='player-icons music-player-icons tooltip' tooltip-txt='재생하기'> <Start size={"16"} /> </div>
-          <div className='player-icons tooltip' tooltip-txt='다음'> <Previous size={"16"} /> </div>
-          <div className='player-icons tooltip' tooltip-txt='반복 활성화하기'> <Loop size={"16"} /> </div>
+          {player_controls}
         </div>
         <div id='player-bar'>
           <p className='player-bar-time'> -:-- </p>
@@ -21,16 +53,7 @@ function MusicPlayer() {
         </div>
       </div>
       <div id='right-controls'>
-        <div className='player-icons tooltip' tooltip-txt='지금 재생 중 보기'> <NowPlaying size={"16"} /> </div>
-        <div className='player-icons tooltip' tooltip-txt='가사'> <Lyrics size={"16"} /> </div>
-        <div className='player-icons tooltip' tooltip-txt='재생목록'> <Playlist size={"16"} /> </div>
-        <div className='player-icons tooltip' tooltip-txt='기기에 연결하기'> <Connect size={"16"} /> </div>
-        <div id='right-sound-controls'>
-          <div className='player-icons tooltip' tooltip-txt='음소거하기'> <Sound size={"16"} /> </div>
-          <div id='controls-bar-sound' className='controls-bar' /> {/* 아이거어떡하지 */}
-        </div>
-        <div className='player-icons tooltip' tooltip-txt='미니플레이어 열기'> <PIP size={"16"} /> </div>
-        <div className='player-icons tooltip' tooltip-txt='전체 화면으로 전환'> <FullScreen size={"16"} /> </div> {/* tooltip 화면 넘을 떄 어떻게 처리할건지 */}
+        {right_controls}
       </div>
     </div>
   )
