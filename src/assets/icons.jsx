16b39c01f4fe4c0ae2dc3d24819ffda6c6d56ae8
shusbox <shusbox@naver.com>
2025-09-16 22:57:04 +0900

    fix musicPlayer

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -14,6 +14,8 @@ export function Social({ size }) { return <svg width={size} height={size} viewBo
 
 export function Pause({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16"><path fill="currentColor" d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7z"/></svg> }
 
+export function Start({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16" fill="currentColor"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288z"/></svg> }
+
 export function Play({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16"><path fill="currentColor" d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7z"/></svg> }
 
 export function Previous({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16"><path fill="currentColor" d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7z"/></svg> }
--- src/components/musicPlayer.jsx
+++ src/components/musicPlayer.jsx
@@ -1,4 +1,4 @@
-import { Shuffle, Loop, Pause, Play, Previous, NowPlaying, Lyrics, Playlist, Sound, PIP, FullScreen, Connect } from '../assets/icons.jsx'
+import { Shuffle, Loop, Pause, Start, Play, Previous, NowPlaying, Lyrics, Playlist, Sound, PIP, FullScreen, Connect } from '../assets/icons.jsx'
 import '../css/musicPlayer.css'
 
 function MusicPlayer() {
@@ -7,11 +7,12 @@ function MusicPlayer() {
       <div></div>
       <div id='playing-selection'>
         <div id='player-controls'>
-          <div className='player-icons'> <Shuffle size={"16"} /> </div>
-          <div className='player-icons'> <Play size={"16"} /> </div>
-          <div id='player-icons-pause' className='player-icons music-player-icons'> <Pause size={"16"} /> </div>
-          <div className='player-icons'> <Previous size={"16"} /> </div>
-          <div className='player-icons'> <Loop size={"16"} /> </div>
+          <div className='player-icons tooltip' tooltip-txt='셔플 활성화하기'> <Shuffle size={"16"} /> </div>
+          <div className='player-icons tooltip' tooltip-txt='이전'> <Play size={"16"} /> </div>
+          {/* 누르면 시작하고 더ㅏ시 누르면 멈출수있게 멈추면 아이콘이랑 content도 바뀌게 */}
+          <div id='player-icons-pause' className='player-icons music-player-icons tooltip' tooltip-txt='재생하기'> <Start size={"16"} /> </div>
+          <div className='player-icons tooltip' tooltip-txt='다음'> <Previous size={"16"} /> </div>
+          <div className='player-icons tooltip' tooltip-txt='반복 활성화하기'> <Loop size={"16"} /> </div>
         </div>
         <div id='player-bar'>
           <p className='player-bar-time'> -:-- </p>
@@ -20,16 +21,16 @@ function MusicPlayer() {
         </div>
       </div>
       <div id='right-controls'>
-        <div className='player-icons'> <NowPlaying size={"16"} /> </div>
-        <div className='player-icons'> <Lyrics size={"16"} /> </div>
-        <div className='player-icons'> <Playlist size={"16"} /> </div>
-        <div className='player-icons'> <Connect size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='지금 재생 중 보기'> <NowPlaying size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='가사'> <Lyrics size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='재생목록'> <Playlist size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='기기에 연결하기'> <Connect size={"16"} /> </div>
         <div id='right-sound-controls'>
-          <div className='player-icons'> <Sound size={"16"} /> </div>
+          <div className='player-icons tooltip' tooltip-txt='음소거하기'> <Sound size={"16"} /> </div>
           <div id='controls-bar-sound' className='controls-bar' /> {/* 아이거어떡하지 */}
         </div>
-        <div className='player-icons'> <PIP size={"16"} /> </div>
-        <div className='player-icons'> <FullScreen size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='미니플레이어 열기'> <PIP size={"16"} /> </div>
+        <div className='player-icons tooltip' tooltip-txt='전체 화면으로 전환'> <FullScreen size={"16"} /> </div> {/* tooltip 화면 넘을 떄 어떻게 처리할건지 */}
       </div>
     </div>
   )
