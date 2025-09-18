e65bdb8c83339954b6d4d501be0a446b20b854c2
shusbox <shusbox@naver.com>
2025-09-18 17:45:38 +0900

    separate files

---
+++ src/components/content.jsx
@@ -0,0 +1,89 @@
+import { Start } from '../assets/icons.jsx'
+import { cover1, cover2, cover3, cover4, cover5, cover6, cover7, cover8 } from '../assets/cover/index.jsx'
+import '../css/content.css'
+
+function Content() {
+  function ContentMainPlaylist({ img, txt }) {
+    return (
+      <div className='content-main-playlist'>
+        <div className='content-main-cover-container'>              
+          <div className='content-main-icons'> <Start size={"20"} /> </div>
+          <img className='content-main-cover' src={img} />
+        </div>
+        <p className='content-main-txt'> {txt} </p>
+      </div>
+    )
+  }
+  const content_main_playlist_arr_1 = [
+    { type: "playlist", img: cover1, txt: "Neckwav 및 TWICE" },
+    { type: "playlist", img: cover2, txt: "AIRI KANNA, 스텔라이브 및 HoneyChurros" }
+  ];
+
+  const content_main_playlist_arr_2 = [
+    { type: "playlist", img: cover3, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" },
+    { type: "playlist", img: cover4, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" },
+    { type: "playlist", img: cover5, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" },
+    { type: "playlist", img: cover6, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" },
+    { type: "playlist", img: cover7, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" },
+    { type: "playlist", img: cover8, txt: "현재 가장 많이 재생된 트랙의 일일 업데이트입니다" }
+  ];
+
+  const content_main_playlist_1 = content_main_playlist_arr_1.map((item, index) => {
+    return (
+      <div key={index} className='content-main-playlist'>
+        <div className='content-main-cover-container'>              
+          <div className='content-main-icons'> <Start size={"20"} /> </div>
+          <img className='content-main-cover' src={item.img} />
+        </div>
+        <p className='content-main-txt'> {item.txt} </p>
+      </div>
+    )
+  });
+
+  const content_main_playlist_2 = content_main_playlist_arr_2.map((item, index) => {
+    return <ContentMainPlaylist key={index} img={item.img} txt={item.txt} />
+  });
+
+  const playlist_num = {
+    1: content_main_playlist_1,
+    2: content_main_playlist_2
+  };
+
+  function ContentMainPlaylistContainer({ num, title }) {
+    const recall = playlist_num[num];
+    return (
+      <div className='content-main-suggestion-container'>
+        <div className='content-main-suggestion-im'>
+          <h2 className='content-main-suggestion'> {title} </h2>
+          <p className='content-main-suggestion-txt'> 모두 표시 </p>
+        </div>
+        <div id='content-main-playlist-container'>
+          {recall}
+        </div>      
+      </div>
+    )
+  }
+
+  return (
+    <div id='content'>
+      <div id='content-header'>
+        <button id='content-header-select' className='content-header-selecter'> 모두 </button>
+        <button className='content-header-selecter'> 음악 </button>
+        <button className='content-header-selecter'> 팟캐스트 </button>
+      </div>
+      <div id='content-main'>
+        <ContentMainPlaylistContainer num={1} title={"들어볼 만한 음악"} />
+        <ContentMainPlaylistContainer num={2} title={"추천 차트"} />
+      </div>
+      <footer>
+        <div className='content-line' />
+        <div>
+          
+        </div>
+        <div className='content-line' />
+      </footer>
+    </div>
+  )
+}
+
+export default Content
