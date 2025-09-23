a0db4acf8d15e357f118cf3f9f2af1703181bfe2
shusbox <shusbox@naver.com>
2025-09-23 15:16:51 +0900

    오

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -43,3 +43,5 @@ export function FullScreen({ size }) { return <svg width={size} height={size} vi
 export function Add({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16" fill="currentColor"><path d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75"/></svg> }
 
 export function Hide({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M10.97 5.47a.75.75 0 1 1 1.06 1.06L10.56 8l1.47 1.47a.75.75 0 1 1-1.06 1.06l-2-2a.75.75 0 0 1 0-1.06z"/><path d="M1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm.5 1.5H5v13H1.5zm13 13h-8v-13h8z"/></svg> }
+
+export function LibraryFullScreen({ size }) { return <svg width={size} height={size} viewBox="0 0 16 16" fill="currentColor"><path d="M6.53 9.47a.75.75 0 0 1 0 1.06l-2.72 2.72h1.018a.75.75 0 0 1 0 1.5H1.25v-3.579a.75.75 0 0 1 1.5 0v1.018l2.72-2.72a.75.75 0 0 1 1.06 0zm2.94-2.94a.75.75 0 0 1 0-1.06l2.72-2.72h-1.018a.75.75 0 1 1 0-1.5h3.578v3.579a.75.75 0 0 1-1.5 0V3.81l-2.72 2.72a.75.75 0 0 1-1.06 0"/></svg> }
--- src/components/library.jsx
+++ src/components/library.jsx
@@ -1,45 +1,33 @@
+import styled from "styled-components";
 import { Add, Hide } from '../assets/icons'
 import '../css/library.css'
 
-function Library() {
-  function LibraryBox({ title, txt, btn }) {
-    return (
-      <div className='library-box'>
-        <div className='library-box-title'>
-          <h3> {title} </h3>
-          <p> {txt} </p>
-        </div>
-        <div className='library-box-btn'> <p> {btn} </p> </div>
-      </div>
-    )
-  }
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
+function Library() {
   return (
     <div id='library'>
-      <div id='library-title'>
-        <div id='library-title-txt-container'>
+      <Flex id='library-title'>
+        <Flex id='library-title-txt-container'>
           <div id='library-title-txt-icons'>
             <Hide size={"16"} />
           </div>
           <h2 id='library-title-txt'> 내 라이브러리 </h2>
+        </Flex>
+        <div>
+          <Flex id='library-create'>
+            <Add size={"16"} />
+            <p id='library-create-txt'> 만들기 </p>
+          </Flex>
+          <div id='library-fullscreen'></div>
         </div>
-        <div id='library-create'>
-          <Add size={"16"} />
-          <p id='library-create-txt'> 만들기 </p>
-        </div>
-      </div>
-      <div id='library-container'>
-        <LibraryBox
-          title={"첫 번째 플레이리스트를 만드세요."}
-          txt={"어렵지 않아요, 저희가 도와드릴게요."}
-          btn={"플레이리스트 만들기"}
-        />
-        <LibraryBox
-          title={"팔로우할 팟캐스트를 찾아보세요"}
-          txt={"새로운 에피소드에 대한 소식을 알려드릴게요."}
-          btn={"팟캐스트 둘러보기"}
-        />
-      </div>
+      </Flex>
+      <Flex id='library-container'>
+
+      </Flex>
     </div>
   )
 }
