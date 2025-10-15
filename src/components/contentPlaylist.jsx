6c2fe3efa0a504728a3f2938c14ce4d9eeeed15b
shusbox <shusbox@naver.com>
2025-10-15 20:24:52 +0900

    플레이리스트

--- src/components/contentPlaylist.jsx
+++ src/components/contentPlaylist.jsx
@@ -6,14 +6,16 @@ const ContentPlaylist = () => {
   return (
     <Content id='playlist-content'>
       <Flex id='playlist-content-title'>
-        <Txt> 재생목록 </Txt>
-        <Close size={"16"} />
+        <p id='playlist-content-title-txt'> 재생목록 </p>
+        <div id='playlist-content-title-icon'> <Close size={"16"} /> </div>
       </Flex>
       <Flex id='playlist-content-subtitle'>
         <Playlist size={"48"} />
-        <Title> 재생 목록에 추가하기 </Title>
-        <Txt> 여기에서 보려면 트랙의 메뉴에서 '재생목록에 추가'를 누르세요. </Txt>
-        <button> 재생할 음악 찾기 </button>
+        <div>
+          <h1 id='playlist-content-subtitle-title'> 재생 목록에 추가하기 </h1>
+          <p id='playlist-content-subtitle-txt'> 여기에서 보려면 트랙의 메뉴에서 '재생목록에 추가'를 누르세요. </p>
+        </div>
+        <button id='playlist-content-btn'> 재생할 음악 찾기 </button>
       </Flex>
     </Content>
   )
